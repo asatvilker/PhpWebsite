7318f7565f7061fcd5926489c45c7d45e7b3f6f2
asatvilker <asatvilker@users.noreply.github.com>
2020-04-24 14:27:36 +0100

    Update login.php

--- webroot/mini_project/login.php
+++ webroot/mini_project/login.php
@@ -13,11 +13,13 @@ if ($conn->connect_error) {
 if ($_SERVER['REQUEST_METHOD'] == POST){
 
  $sql = "SELECT * FROM USERS WHERE email = $_POST['Email'] AND password = $_POST['Password']";
- if ($conn->query($sql) === TRUE) {
-      require(addpost.html);
- } else {
- echo "Error: " . $sql . "<br>" . $conn->error;
- }
- $conn->close();
- }
- ?>
+ $result = $conn->query($sql);
+
+ if ($result->num_rows > 0) {
+    // output data of each row
+    require 'addpost.html';
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
