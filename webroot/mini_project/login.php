c186fdb8e05a6005a283b7ff8dd52c6129e4e7c0
asatvilker <asatvilker@users.noreply.github.com>
2020-04-24 23:31:30 +0100

    Update login.php

--- webroot/mini_project/login.php
+++ webroot/mini_project/login.php
@@ -1,29 +1,74 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("MYSQL_USER");
-$dbpwd = getenv("MYSQL_PASSWORD");
-$dbname = getenv("MYSQL_DATABASE");
+$dbuser = 'root';
+$dbpwd = '';
+$dbname = 'USERS';
 // Creates connection
 $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+
 // Checks connection
 if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
 }
-if ($_SERVER['REQUEST_METHOD'] == POST){
-
- $sql = "SELECT * FROM USERS";
- $result = $conn->query($sql);
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+$email=$_POST['Email'];
+$pass=$_POST['Password'];
+$query = "SELECT * FROM DETAILS WHERE email LIKE '".$_POST['Email']."' AND password LIKE '".$_POST['Password']."'";
+ $result = $conn->query($query);
 
  if ($result->num_rows > 0)
  {
+
     // output data of each row
     require 'addpost.html';
   }
   else
   {
-    echo "0 results";
+    ?>
+    <html>
+    <head lang="en">
+       <meta charset="utf-8">
+       <title>Layouts</title>
+       <link rel="stylesheet" href="reset.css" type="text/css"/>
+       <link rel="stylesheet" href="login.css" type="text/css"/>
+       <style>
+       .error
+       {
+         color: red;
+         position: absolute;
+         left: 21em;
+         top: 31em;
+       }
+       </style>
+    </head>
+    <body>
+
+      <article>
+        <h2>Sign in</h2>
+        <h3 class="error">Please eneter valid username and password</h3>
+        <form action="login.php" method="post">
+          <p>
+
+            <input type="email" name="Email" required placeholder="Email">
+          </p>
+          <p>
+
+            <input type="password" name="Password" minlength="8" required placeholder="Password">
+          </p>
+          <p>
+          <input type="submit" value="Login"  class="button" >
+
+          </p>
+        </form>
+
+      </article>
+
+    </body>
+    </html>
+    <?php
   }
 }
+
 $conn->close();
 ?>
