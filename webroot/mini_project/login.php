0642519e33da9ae17f74bb3491c0a77b78b5f6e2
asatvilker <asatvilker@users.noreply.github.com>
2020-04-24 14:53:01 +0100

    Update login.php

--- webroot/mini_project/login.php
+++ webroot/mini_project/login.php
@@ -1,9 +1,9 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("DATABASE_USER");
-$dbpwd = getenv("DATABASE_PASSWORD");
-$dbname = getenv("DATABASE_NAME");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
 // Creates connection
 $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 // Checks connection
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 }
 if ($_SERVER['REQUEST_METHOD'] == POST){
 
- $sql = "SELECT * FROM USERS WHERE email = $_POST['Email'] AND password = $_POST['Password']";
+ $sql = "SELECT * FROM USERS";
  $result = $conn->query($sql);
 
  if ($result->num_rows > 0) {
