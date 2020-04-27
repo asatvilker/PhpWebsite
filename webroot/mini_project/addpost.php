8b76a8affe923b4276344fb517228cb8ed4de1a3
asatvilker <asatvilker@users.noreply.github.com>
2020-04-28 00:02:59 +0100

    .

--- webroot/mini_project/addpost.php
+++ webroot/mini_project/addpost.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 $temp= date("l jS \of F Y h:i:s A");
 $temp1= strval($temp);
-$sql= "INSERT INTO BLOG_TABLE ('Title', 'Blog' , 'Date') VALUES ('".$_POST['title']."','".$_POST['blog']."','".$temp1."')";
+$sql= "INSERT INTO BLOGS_TABLE (Title, Blog) VALUES ('".$_POST['title']."','".$_POST['blog']."')";
 
   if($conn->query($sql))
   {
